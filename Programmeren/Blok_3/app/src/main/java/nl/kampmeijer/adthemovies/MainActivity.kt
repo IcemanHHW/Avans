f40d61e04d5b5322aab2d1b21320b3a61ac8ebff
Maarten <maartenkampmeijer@hotmail.com>
2026-02-03 11:21:16 +0100

    Programmeren 3 deel 1

---
+++ Programmeren/Blok_3/app/src/main/java/nl/kampmeijer/adthemovies/MainActivity.kt
@@ -0,0 +1,30 @@
+package nl.kampmeijer.adthemovies
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val b: Button;
+        b = findViewById(R.id.button);
+        b.setOnClickListener {
+            val tv: TextView;
+            tv = findViewById(R.id.textView);
+            tv.setText("adTheMovies");
+        }
+    }
+}
